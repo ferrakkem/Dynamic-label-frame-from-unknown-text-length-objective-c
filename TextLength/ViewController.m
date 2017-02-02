9ea9fc5c07e742c7360d505e23d97f8631c3da36
ferrakkem <ferrakkem@gmail.com>
2017-02-02 12:16:18 +0600

    Dynamic label frame from unknown text length

--- TextLength/ViewController.m
+++ TextLength/ViewController.m
@@ -9,14 +9,49 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UILabel *myLabel;
+}
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+#pragma mark ::: call_createLabel
+    [self createLabel];
+}
+
+#pragma mark ::: Create Label
+-(void)createLabel
+{
+    NSLog(@"HI");
+    myLabel = [[UILabel alloc] init];
+    NSString *messages = @"Some dynamic text for label";
+    
+    //set the text and style if any
+    myLabel.text = messages;
+    myLabel.numberOfLines = 0;
+    
+    CGSize maximumLabelSize = CGSizeMake(280, 9999); //280:max width of label and 9999-max height of label.
+    
+    // use font information from the UILabel to calculate the size
+    CGSize expectedLabelSize = [myLabel sizeThatFits:maximumLabelSize];
+    
+    // create a frame that is filled with the UILabel frame data
+    CGRect newFrame = myLabel.frame;
+    
+    // resizing the frame to calculated size
+    newFrame.size.height = expectedLabelSize.height;
+    
+    // put calculated frame into UILabel frame
+    myLabel.frame = newFrame;
+    
+    [self.view addSubview:myLabel];
+    
 }
 
 
